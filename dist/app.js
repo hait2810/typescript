a99996d73f380d91334615c793226cb89f49ee4d
hait2810 <checked.kenvip10@gmail.com>
2022-03-08 18:11:27 +0700

    update

---
+++ dist/app.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello typescript");
+const a = 1;
+const myName = "Nguyen Van Hai";
+const status = true;
+const myObj = {};
+const numberArr = [1, 2, 3, 4];
+const stringArr = ["Hai", "A"];
+const objectArr = [{ id: 1, name: "Hai" }, { id: 2, name: "Hải" }];
+// tsc -w tên file
+/**
+ * tsc --init
+ * target: "es2018"
+ * baseUrl: "./src"
+ * outDir: "./dist
+ *  "sourceMap": true,
+ */ 
+//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,26 @@
-console.log("Hello typescript");
 
+export {};
+console.log("Hello typescript");
+// khai báo 
+type User = {
+    id:number;
+     name: string;
+     status?: boolean; //required optional chaining
+}
+const a:number = 1;
+const myName: string = "Nguyen Van Hai";
+const status : boolean = true;
+const myObj: object = {};
+const numberArr:number[] = [1,2,3,4];
+const stringArr:string[] = ["Hai","A"];
+const objectArr:User[] = [{id:1,name:"Hai"},{id:2,name:"Hải"}];
 
 
 // tsc -w tên file
+/**
+ * tsc --init
+ * target: "es2018"
+ * baseUrl: "./src"
+ * outDir: "./dist
+ *  "sourceMap": true,  
+ */
